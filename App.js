1a1ec70da0851dfc22ffca2040ae3d517408bb1f
hectorivanab-dot <hectorivanab@gmail.com>
2026-06-19 14:19:40 -0500

    hotfix: agregado banner de mantenimiento de emergencia por qa

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import { Text, View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hola Mundo</Text>
+      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Aplicación en mantenimiento</Text>
     </View>
   );
 }
